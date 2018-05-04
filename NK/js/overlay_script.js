6a8a2e8ce0eaecbbbb4f4ddf755a04bf0f790a98
Arun Prasaad Gunasekaran <arunprasaad2711@gmail.com>
2018-05-05 04:49:55 +0530

    Added all files

---
+++ NK/js/overlay_script.js
@@ -0,0 +1,17 @@
+/* 
+    Overlay script.
+
+    Used for display the overlay menu.
+
+*/
+
+
+function openNav()
+{
+    document.getElementById("navOverlay").style.display = "block";
+}
+
+function closeNav()
+{
+    document.getElementById("navOverlay").style.display = "none";
+}
